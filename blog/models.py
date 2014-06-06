ee8ccb767c0e3504f1d9bf27d4afa6028945ce34
Masud <masud@karitkarma.com>
2014-06-06 10:03:01 +0600

    added post class for in model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
+
+class Post(models.Model):
+    """docstring for ClassName"""
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    Created = models.DateTimeField()
+    photo = models.ImageField(upload_to="image")
+    tags = TaggableManager()
+
+    class Meta:
+        verbose_name_plural = "image"
+    def __unicode__(self):
+        return self.title
+
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+
